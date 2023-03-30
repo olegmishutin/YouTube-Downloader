a0a3f2b809274a46e3d3358b74b6d128eaf7b4d0
OlegMishutin <omisutin646@gmail.com>
2023-03-30 17:12:09 +0300

    linux v1.1
    - Added animations for input fields.
    - The scrollbar has been styled.
    - The application icon has been drawn and added to the HTML code.
    - Added display of information about the video when entering a link
    - Removed automatic deletion of link and status
    - Added button to open saving folder
    - Fixed graphical bug when bootloader status appears
    - Code improvement
    - Updated README

--- Interface/JsInterlayer/folderSelector.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('openFolder').onclick = SelectFolder
-
-async function SelectFolder() {
-    let path = await eel.SelectFolder()();
-    document.getElementById("pathTextBox").value = path;
-}
