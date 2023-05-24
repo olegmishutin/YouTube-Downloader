4e5b3cfa1ee9bccf67fe21d4fd8abcf66d05d26a
OlegMishutin <omisutin646@gmail.com>
2023-05-24 18:22:15 +0300

    windows v1.3
    - Added light theme and theme switcher
    - Minor interface changes
    - Code improvement

--- Interface/JsInterlayer/folderManager.js
+++ Interface/JsInterlayer/folderManager.js
@@ -1,6 +1,5 @@
 document.getElementById("select-folder").onclick = GetPathForDownloading;
 document.getElementById("open-saving-path").onclick = OpenSavingPath;
-
 const path = document.getElementById("path-input-field");
 
 async function GetPathForDownloading() {
